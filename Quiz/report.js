b5cf79a343799fb6ac64f675c6f8054fad82c21d
Hemant Modi <90129538+HemantModi11@users.noreply.github.com>
2023-09-24 02:15:01 +0530

    Add files via upload

---
+++ Quiz/report.js
@@ -0,0 +1,196 @@
+let reportData = {
+    categories: [
+      {
+        name: "Stress",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "You're effectively managing stress in your life. Continue practicing stress reduction techniques to maintain your well-being. Consider sharing your stress management tips with others.",
+            solutions: [
+              "Keep up with your stress-reduction practices for continued well-being.",
+              "Incorporate relaxation exercises into your routine.",
+              "Prioritize self-care to reduce stress levels."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "You might consider incorporating relaxation exercises into your routine. Explore stress-reduction techniques like mindfulness and deep breathing. Make time for self-care to reduce stress levels.",
+            solutions: [
+              "Explore relaxation exercises to reduce stress.",
+              "Incorporate mindfulness and meditation into your routine.",
+              "Make time for self-care to manage stress."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "High stress levels may be affecting your overall well-being. Seek professional help or counseling to address stress-related concerns. Prioritize self-care and stress management strategies.",
+            solutions: [
+              "Seek professional help to address high stress levels.",
+              "Implement stress management strategies in your daily life.",
+              "Prioritize self-care for your overall well-being."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Anxiety",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "Your anxiety levels appear to be well-managed. Continue using healthy coping mechanisms and stress-reduction techniques to maintain your well-being.",
+            solutions: [
+              "Practice mindfulness and relaxation techniques regularly.",
+              "Engage in physical activity to reduce anxiety.",
+              "Maintain a support network of friends and family."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "You may be experiencing moderate anxiety levels. Consider incorporating relaxation exercises, mindfulness, or deep breathing into your routine to manage anxiety.",
+            solutions: [
+              "Explore relaxation exercises and mindfulness practices.",
+              "Create a daily routine that includes stress-reduction activities.",
+              "Reach out to friends or a therapist for support."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "High anxiety levels could be negatively impacting your well-being. It's advisable to seek professional help or counseling to address anxiety-related concerns.",
+            solutions: [
+              "Consult with a mental health professional for guidance.",
+              "Implement anxiety management strategies in your daily life.",
+              "Prioritize self-care and stress reduction."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Social Pressure",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "You seem to handle social pressures well. Continue to prioritize self-awareness and assertiveness to maintain a healthy balance in social situations.",
+            solutions: [
+              "Maintain a strong sense of self and personal boundaries.",
+              "Practice assertiveness and effective communication.",
+              "Engage in self-care to manage stress."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "You may encounter occasional social pressures. Consider practicing assertiveness and setting boundaries to navigate social situations more comfortably.",
+            solutions: [
+              "Learn assertiveness techniques to manage social pressure.",
+              "Set clear boundaries in social interactions.",
+              "Engage in self-care practices for stress management."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "High social pressure can be challenging. Seek support, such as counseling or therapy, to develop effective coping strategies and assertiveness skills.",
+            solutions: [
+              "Seek professional help for guidance and support.",
+              "Develop assertiveness skills to handle social pressure.",
+              "Prioritize self-care and stress management."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Online Harassment",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "You're effectively managing online harassment and maintaining a positive online presence. Continue practicing digital resilience and promoting a safe online environment.",
+            solutions: [
+              "Maintain strong online privacy settings.",
+              "Promote positive online interactions and report harassment.",
+              "Prioritize digital well-being and self-care."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "You may encounter occasional online harassment. Utilize privacy settings, report harassment, and prioritize online well-being to manage such situations effectively.",
+            solutions: [
+              "Adjust your online privacy settings for added security.",
+              "Report online harassment to platform administrators.",
+              "Engage in digital self-care practices."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "High levels of online harassment can have a negative impact. Consider seeking support and utilizing online safety measures to address harassment issues.",
+            solutions: [
+              "Seek professional support for online harassment trauma.",
+              "Enhance online safety measures and privacy settings.",
+              "Prioritize digital self-care and mental well-being."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Well Being",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "Your overall well-being appears to be positive. Continue engaging in self-care activities, maintaining a strong support network, and pursuing your interests for continued well-being.",
+            solutions: [
+              "Prioritize self-care and engage in activities you enjoy.",
+              "Cultivate and maintain a strong support network.",
+              "Explore new hobbies and interests for personal growth."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "You may experience occasional dips in well-being. Consider focusing on self-care, seeking support when needed, and exploring new activities for overall well-being.",
+            solutions: [
+              "Enhance self-care practices for improved well-being.",
+              "Reach out to friends and loved ones for support.",
+              "Explore new interests and activities for personal fulfillment."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "Your overall well-being may need attention. It's essential to prioritize self-care, seek support, and explore activities that promote positive mental health.",
+            solutions: [
+              "Prioritize self-care as a crucial aspect of your daily routine.",
+              "Connect with mental health professionals for guidance.",
+              "Engage in activities that promote mental and emotional well-being."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Information Overload",
+        assessmentLevels: [
+          {
+            level: "Good",
+            content: "You appear to manage information overload well. Continue practicing digital mindfulness, setting limits, and organizing your digital activities for a balanced online experience.",
+            solutions: [
+              "Practice digital mindfulness and set boundaries with technology.",
+              "Organize your online activities for efficiency.",
+              "Maintain a healthy balance between online and offline life."
+            ]
+          },
+          {
+            level: "Moderate",
+            content: "Occasional struggles with information overload may occur. Consider implementing digital organization strategies and mindfulness techniques to maintain a healthier digital lifestyle.",
+            solutions: [
+              "Organize your digital activities and prioritize essential tasks.",
+              "Practice mindfulness to manage information consumption.",
+              "Set limits on screen time for a balanced life."
+            ]
+          },
+          {
+            level: "Poor",
+            content: "High information overload can negatively impact your digital well-being. Explore digital detox options, seek support, and establish healthy online routines.",
+            solutions: [
+              "Consider a digital detox to reset your online habits.",
+              "Connect with professionals for guidance on digital well-being.",
+              "Establish daily routines that promote a healthier online experience."
+            ]
+          }
+        ]
+      }
+    ]
+  };
